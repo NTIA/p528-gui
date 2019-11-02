1c68b31fe70c9d1bd84e93c2f3e5d6a660be09e5
Billy Kozma <wkozma@ntia.doc.gov>
2019-11-02 14:24:11 -0600

    Updated how plot label is formatted to avoid over-rounding distance values

--- win32/PointViewModel.cs
+++ win32/PointViewModel.cs
@@ -10,14 +10,14 @@ namespace p528_gui
     public class PointViewModel
     {
         public string PropMode { get; set; }
-        public string DistanceKM { get; set; }
+        public string Distance { get; set; }
         public string LossDB { get; set; }
         public Brush Color { get; set; }
 
         public PointViewModel(string propMode, double d__km, double A__db, Brush color, Units units)
         {
             this.PropMode = propMode;
-            this.DistanceKM = $"{Math.Round(d__km, 0).ToString("F0")} " + ((units == Units.Meters) ? "km" : "n miles");
+            this.Distance = $"{Math.Round(d__km, 2).ToString("F2")} " + ((units == Units.Meters) ? "km" : "n miles");
             this.LossDB = $"{ Math.Round(A__db, 3).ToString("F3")} dB";
             this.Color = color;
         }
