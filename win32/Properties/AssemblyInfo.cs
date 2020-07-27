be767b3c1af8faf9c0dbf56b2de8ad03ebfa0c31
Billy Kozma <wkozma@ntia.doc.gov>
2020-07-27 16:42:33 -0600

    bumping version number to prep for re-releasing with signed executable

--- win32/Properties/AssemblyInfo.cs
+++ win32/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ITS ITU-R Rec P.528-4 GUI")]
+[assembly: AssemblyTitle("ITS Rec ITU-R P.528-4 GUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Institute for Telecommunication Sciences")]
 [assembly: AssemblyProduct("p528-gui")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
