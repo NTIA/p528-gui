752e5551e89be5f457c7748169a4c2b968887ee6
Billy Kozma <wkozma@ntia.doc.gov>
2022-01-20 08:04:43 -0700

    removed unused warning message

--- win32/Messages.cs
+++ win32/Messages.cs
@@ -5,8 +5,6 @@
         // warning text
         public const string ModelConsistencyWarning = "Note: The P.528 model has returned a warning that the transition between diffraction and troposcatter might not be physically consistent.  Care should be taken when using the results.";
 
-        public const string TerminalHeightWarning = "Note: Although valid, the entered value is above the reference atmosphere which stops at 475 km above sea level";
-
         public const string Terminal1LessThan2Error = "Terminal 1 must be less than the height of Terminal 2";
 
         public const string FrequencyRangeError = "The frequency must be between 100 MHz and 30 000 MHz, inclusive";
